d7543b37a0ece9a630066a11b8f02ccdfaa4938f
Nankai <vn.apnic@gmail.com>
2020-12-24 11:09:55 +0700

    add flavor for IOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,17 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    
+    // custom code to allow get flavor
+    let controller = self.window.rootViewController as! FlutterViewController
+    let channel = FlutterMethodChannel.init(name: "flavor", binaryMessenger: controller.binaryMessenger)
+    channel.setMethodCallHandler { (call, result) in
+        // Note: this method is invoked on the UI thread
+        // Handle battery messages.
+        let flavor = Bundle.main.infoDictionary?["Flavor"]
+        result(flavor)
+    }
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
