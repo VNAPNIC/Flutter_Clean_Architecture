deb56ae69c8a73aaad7b85631c3ec8176fc2f658
DESKTOP-5I89Q3O\vnapn <vn.apnic@Gmail.com>
2022-02-23 18:33:05 +0700

    update android 31

--- android/app/src/main/kotlin/com/vnapnic/flutter_architecture/MainActivity.kt
+++ android/app/src/main/kotlin/com/vnapnic/flutter_architecture/MainActivity.kt
@@ -8,6 +8,9 @@ class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         // custom method to get flavor
-        MethodChannel(flutterEngine.dartExecutor, "flavor").setMethodCallHandler { _, result -> result.success(BuildConfig.FLAVOR) }
+        MethodChannel(
+            flutterEngine.dartExecutor,
+            "flavor"
+        ).setMethodCallHandler { _, result -> result.success(BuildConfig.FLAVOR) }
     }
 }
